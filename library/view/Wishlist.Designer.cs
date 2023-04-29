98c1235421aa3a725d12497d06572af8c3b1c2d2
Ioana Simtion <ionitza6@gmail.com>
2023-04-29 14:44:36 +0300

    added main menu

--- library/view/Wishlist.Designer.cs
+++ library/view/Wishlist.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace library.view
 {
-    partial class Home
+    partial class Wishlist
     {
         /// <summary>
         /// Required designer variable.
@@ -28,29 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTitle = new System.Windows.Forms.Label();
             this.btnCreate = new System.Windows.Forms.Button();
             this.lstBooks = new System.Windows.Forms.ListView();
+            this.btnRemoveBook = new System.Windows.Forms.Button();
+            this.btnUpdateBook = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblTitle.Location = new System.Drawing.Point(37, 40);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(88, 32);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Books";
             // 
             // btnCreate
             // 
             this.btnCreate.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCreate.Location = new System.Drawing.Point(37, 91);
+            this.btnCreate.Location = new System.Drawing.Point(755, 292);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(202, 48);
+            this.btnCreate.Size = new System.Drawing.Size(202, 45);
             this.btnCreate.TabIndex = 1;
-            this.btnCreate.Text = "Create New Book";
+            this.btnCreate.Text = "Add Book";
             this.btnCreate.UseVisualStyleBackColor = true;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
@@ -58,33 +49,53 @@
             // 
             this.lstBooks.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.lstBooks.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lstBooks.Location = new System.Drawing.Point(37, 158);
+            this.lstBooks.Location = new System.Drawing.Point(12, 12);
             this.lstBooks.Name = "lstBooks";
-            this.lstBooks.Size = new System.Drawing.Size(945, 361);
+            this.lstBooks.Size = new System.Drawing.Size(945, 259);
             this.lstBooks.TabIndex = 3;
             this.lstBooks.UseCompatibleStateImageBehavior = false;
             this.lstBooks.View = System.Windows.Forms.View.Details;
             this.lstBooks.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
-            // Home
+            // btnRemoveBook
+            // 
+            this.btnRemoveBook.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRemoveBook.Location = new System.Drawing.Point(755, 343);
+            this.btnRemoveBook.Name = "btnRemoveBook";
+            this.btnRemoveBook.Size = new System.Drawing.Size(202, 45);
+            this.btnRemoveBook.TabIndex = 4;
+            this.btnRemoveBook.Text = "Remove Book";
+            this.btnRemoveBook.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateBook
+            // 
+            this.btnUpdateBook.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdateBook.Location = new System.Drawing.Point(755, 395);
+            this.btnUpdateBook.Name = "btnUpdateBook";
+            this.btnUpdateBook.Size = new System.Drawing.Size(202, 40);
+            this.btnUpdateBook.TabIndex = 5;
+            this.btnUpdateBook.Text = "Update Book";
+            this.btnUpdateBook.UseVisualStyleBackColor = true;
+            // 
+            // Wishlist
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1186, 567);
+            this.ClientSize = new System.Drawing.Size(969, 447);
+            this.Controls.Add(this.btnUpdateBook);
+            this.Controls.Add(this.btnRemoveBook);
             this.Controls.Add(this.lstBooks);
             this.Controls.Add(this.btnCreate);
-            this.Controls.Add(this.lblTitle);
-            this.Name = "Home";
-            this.Text = "Home";
+            this.Name = "Wishlist";
+            this.Text = "WISHLIST";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label lblTitle;
         private Button btnCreate;
         private ListView lstBooks;
+        private Button btnRemoveBook;
+        private Button btnUpdateBook;
     }
 }
